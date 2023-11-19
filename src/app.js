e076769f71f3fa03312820b82cd86df24c90260c
phoenix1531 <arjun15310@gmail.com>
2023-11-19 12:46:50 +0530

    superwars stage 2 done

--- src/app.js
+++ src/app.js
@@ -21,88 +21,73 @@ const PLAYERS = [
   "Slingo",
 ];
 
-// initialize players with image and strength
 const initPlayers = (players) => {
   let detailedPlayers = [];
-  // Create players using for loop
-  // Type your code here
   for (let i = 0; i < players.length; i++) {
-    var singlePlayer = {
+    var currentPlayers = {
+      image: `images/super-${i + 1}.png`,
       name: players[i],
       strength: getRandomStrength(),
       image: `images/super-${i + 1}.png`,
-    //   
       type: getRandomType(i),
     };
-    detailedPlayers.push(singlePlayer);
-    console.log("detailedPlayers: ", detailedPlayers);
+    detailedPlayers.push(currentPlayers);
+    console.log("detailedPlayers", detailedPlayers);
   }
+
   return detailedPlayers;
 };
 
+function getRandomStrength() {
+  return Math.floor(Math.random() * 101);
+}
+
 var heroCount = 0;
 var villainCount = 0;
-
-type = ["hero", "villain"];
+var type = ["hero", "villain"];
 const getRandomType = (i) => {
-    // when i==0, because of test case, first one should be hero
   if (i == 0) {
     heroCount++;
     return "hero";
-  }  
+  }
 
-    // when i>0, rest all are villains
-  var random = Math.floor(Math.random() * type.length);
-  var temptype = type[random];
-  temptype == "hero" ? heroCount++ : villainCount++;
+  var ran = Math.floor(Math.random() * type.length);
+  var tempType = type[ran];
+  tempType == "hero" ? heroCount++ : villainCount++;
   if (heroCount >= 10) {
     return "villain";
   } else if (villainCount >= 10) {
     return "hero";
   }
-  return type[random];
-};
-
-// getting random strength
-const getRandomStrength = () => {
-  // =Return a random integer (0,100]
-  // Note: You can use Math.random() and Math.ceil()
-  var random = Math.floor(Math.random() * 100);
-  return random;
+  return type[ran];
 };
 
 const buildPlayers = (players, type) => {
   let fragment = "";
-  // Loop through players and accumulate HTML template
-  // depending of type of player(hero|villain)
-  // Type your code here
   if (type == "hero") {
-    for (let i = 0; i < players.length; i++) {
+    for (var i = 0; i < players.length; i++) {
       if (players[i].type == "hero") {
-        fragment += ` <div class="player">
-                <img src="${players[i].image}" alt="">
-                <div class="name">${players[i].name}</div>
-                <div class="strength">${players[i].strength}</div>
-            </div>`;
+        fragment += `<div class="player">
+  <img src="${players[i].image}" alt="">
+  <div class="name">${players[i].name}</div>
+  <div class="strength">${players[i].strength}</div>
+</div>`;
       }
     }
-  }
-  if (type == "villain") {
-    for (let i = 0; i < players.length; i++) {
+  } else if (type == "villain") {
+    for (var i = 0; i < players.length; i++) {
       if (players[i].type == "villain") {
-        fragment += ` <div class="player">
-                <img src="${players[i].image}" alt="">
-                <div class="name">${players[i].name}</div>
-                <div class="strength">${players[i].strength}</div>
-            </div>`;
+        fragment += `<div class="player">
+      <img src="${players[i].image}" alt="">
+      <div class="name">${players[i].name}</div>
+      <div class="strength">${players[i].strength}</div>
+   </div>`;
       }
     }
   }
 
-//   console.log("fragment inside BuildPlayers: ", fragment);
   return fragment;
 };
-// Display players in HTML
 const viewPlayers = (players) => {
   document.getElementById("heroes").innerHTML = buildPlayers(players, "hero");
   document.getElementById("villains").innerHTML = buildPlayers(
@@ -110,13 +95,7 @@ const viewPlayers = (players) => {
     "villain"
   );
 };
-TEST_PLAYERS = ["Hero"];
+TEST_PLAYERS = ["hero"];
 window.onload = () => {
   viewPlayers(initPlayers(PLAYERS));
 };
-
-// 1- window.onload
-// 2- initPlayers
-// 3- RandomStrength,randomType
-// 4- DetailedPlayers => viewPlayers
-// 5- BuildPlayers will return either hero or villain html and return it to viewPlayers so all cards can be displayed
